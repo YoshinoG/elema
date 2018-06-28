625af4e2f4a444e25a28c0d377f0ec087eee83ce
gzj <745476203@qq.com>
2018-06-28 21:22:17 +0800

    header

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import Header from './components/header';
 
 
 
 
-ReactDOM.render(<App/>,document.getElementById('out'))
+ReactDOM.render(<Header/>,document.getElementById('out'))
 
