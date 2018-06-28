d4ab221495950672bf2e58fa3af811e822f974f5
gzj <745476203@qq.com>
2018-06-28 22:07:53 +0800

    1.0.1

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import Header from './components/header';
-import Content from './components/content';
+import Content1 from './components/content1';
 
 
 
-ReactDOM.render(<Content><Header/></Content>,document.getElementById('out'));
+ReactDOM.render(<Content1><Header/></Content1>,document.getElementById('out'));
 
